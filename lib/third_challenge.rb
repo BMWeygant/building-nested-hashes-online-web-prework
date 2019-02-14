73941c943364c46c72d854d1f9ccabb4aed2769f
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-14 23:11:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,8 +10,19 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         },
-      :hero_friends => []
-   }, 
+      hero_friends: [
+        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+          },
+        {
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+          }
+        ]
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -26,5 +37,5 @@ def third_challenge
    }
   }
 
-  
+
 end
