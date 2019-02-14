d0f83c48169bd5949005d4ac4d2372965406f153
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-14 23:21:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,7 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      patriarch: {
-        name:
-        age: 
-      },
-      matriarch: {
-        name: "Lady Montague",
-        age: "54"
-      },
-      hero: {
-        name: "Romeo",
-        age: "15"
-      },
+      #your key/value pairs here!
       },
     :capulet => {
       #your key/value pairs here!
